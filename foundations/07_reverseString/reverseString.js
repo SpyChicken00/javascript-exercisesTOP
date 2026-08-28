5d0d76be432ff8c5df144ce23054bc680efe0cf7
SpyChicken00 <94644105+SpyChicken00@users.noreply.github.com>
2026-08-28 21:03:35 +0900

    Complete exercises 6 7 8

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newString = ""
+    const stringArr = string.split("");
+    for (let i = string.length - 1; i >= 0; i--) { //loop from end down
+        newString += stringArr[i]
+    }
+    return newString;
 
+    //alternate solution, convert to array, use arrayReverse
+    //return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    const removeArgs = Array.from(arguments)
+    //loop through removeArgs, for each item remove from the array
+    for (const num of removeArgs){
+        // array.map((item, index) => {
+        //     if (item === num) {
+        //         array.splice(index, 1);
+        //     }}  )
+        for (let i = 0; i < array.length; i++) {
+            let item = array[i];
+            if (item === num) {
+                array.splice(i, 1);
+                i--
+            }
+        }
+    }
+
+    return array;
+    
 };
 
 // Do not edit below this line
